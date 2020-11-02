51b0af06fc51f449b4467ffd8131f1ad454f5a43
tsmr <thetsmr@gmail.com>
2020-11-02 17:50:57 +0100

    Drop version from extract_template.sh

--- tools/extract_template.sh
+++ tools/extract_template.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 soft='GLPI - ManufacturersImports plugin'
-version='2.0.0'
 copyright='INDEPNET Development Team'
 
-xgettext *.php */*.php --copyright-holder='$copyright' --package-name='$soft' --package-version='$version' -o locales/glpi.pot -L PHP --add-comments=TRANS --from-code=UTF-8 --force-po  \
+xgettext *.php */*.php --copyright-holder='$copyright' --package-name='$soft' -o locales/glpi.pot -L PHP --add-comments=TRANS --from-code=UTF-8 --force-po  \
 	--keyword=_n:1,2,4t --keyword=__s:1,2t --keyword=__:1,2t --keyword=_e:1,2t --keyword=_x:1c,2,3t \
 	--keyword=_ex:1c,2,3t --keyword=_nx:1c,2,3,5t --keyword=_sx:1c,2,3t
 
