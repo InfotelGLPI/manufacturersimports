9dec6294fcc6dee3724cd116e591f9467a5294ed
Thetsmr <thetsmr@gmail.com>
2025-09-23 14:11:51 +0200

    Update Github Scripts

--- tools/extract_template.sh
+++ tools/extract_template.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
 
-soft='GLPI - ManufacturersImports plugin'
-copyright='INDEPNET Development Team'
+# Recherche tous les fichiers PHP récursivement
+find . -name '*.php' > php_files.list
 
-xgettext *.php */*.php --copyright-holder='$copyright' --package-name='$soft' -o locales/glpi.pot -L PHP --add-comments=TRANS --from-code=UTF-8 --force-po  \
-	--keyword=_n:1,2,4t --keyword=__s:1,2t --keyword=__:1,2t --keyword=_e:1,2t --keyword=_x:1c,2,3t \
-	--keyword=_ex:1c,2,3t --keyword=_nx:1c,2,3,5t --keyword=_sx:1c,2,3t
+# Extraction avec xgettext
+xgettext --files-from=php_files.list \
+  --copyright-holder='ManufacturersImports Development Team' \
+  --package-name='ManufacturersImports - Accounts plugin' \
+  -o locales/glpi.pot \
+  -L PHP \
+  --add-comments=TRANS \
+  --from-code=UTF-8 \
+  --force-po \
+  --keyword=_n:1,2,4t \
+  --keyword=__s:1,2t \
+  --keyword=__:1,2t \
+  --keyword=_e:1,2t \
+  --keyword=_x:1c,2,3t \
+  --keyword=_ex:1c,2,3t \
+  --keyword=_nx:1c,2,3,5t \
+  --keyword=_sx:1c,2,3t
 
-
-
+# Nettoyage
+rm php_files.list
