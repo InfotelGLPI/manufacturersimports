126018646a2d2665503de395e4883af9724ac81b
Thetsmr <thetsmr@gmail.com>
2026-05-26 10:36:49 +0200

    Update HEADERS

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,5 +1,32 @@
 <?php
 
+/*
+ -------------------------------------------------------------------------
+ manufacturersimports plugin for GLPI
+ Copyright (C) 2015-2026 by the manufacturersimports Development Team.
+
+ https://github.com/InfotelGLPI/manufacturersimports
+ -------------------------------------------------------------------------
+
+ LICENSE
+
+ This file is part of manufacturersimports.
+
+ manufacturersimports is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ manufacturersimports is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with manufacturersimports. If not, see <http://www.gnu.org/licenses/>.
+ --------------------------------------------------------------------------
+ */
+
 define('GLPI_ROOT', dirname(__DIR__, 3));
 
 $loader = require GLPI_ROOT . '/vendor/autoload.php';
