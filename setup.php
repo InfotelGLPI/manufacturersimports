ae6dfb4fb278010be680c0a27d86101aa6164f8d
Thetsmr <thetsmr@gmail.com>
2026-09-07 09:52:29 +0200

    Bump version

--- setup.php
+++ setup.php
@@ -27,7 +27,7 @@
  * --------------------------------------------------------------------------
  */
 
-define('PLUGIN_MANUFACTURERSIMPORTS_VERSION', '3.1.8');
+define('PLUGIN_MANUFACTURERSIMPORTS_VERSION', '3.1.9');
 
 global $CFG_GLPI;
 
